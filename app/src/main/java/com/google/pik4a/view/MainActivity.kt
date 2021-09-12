d4c8ad1dced84a5d9d7a3a8d7e6c52fd390b7e95
ip-droid <89872658+ip-droid@users.noreply.github.com>
2021-09-12 10:58:50 +0300

    добавил fab+bottonappbar вылезла ошибка

--- app/src/main/java/com/google/pik4a/view/MainActivity.kt
+++ app/src/main/java/com/google/pik4a/view/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.google.pik4a.view
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.google.pik4a.R
 import com.google.pik4a.view.picture.PODFragment
 
@@ -9,6 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
 
         if(savedInstanceState==null){
             supportFragmentManager.beginTransaction().replace(R.id.container, PODFragment.newInstance()).commit()
--- app/src/main/java/com/google/pik4a/view/picture/PODFragment.kt
+++ app/src/main/java/com/google/pik4a/view/picture/PODFragment.kt
@@ -8,13 +8,16 @@ import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coil.load
+import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.pik4a.R
 import com.google.pik4a.databinding.FragmentMainBinding
+import com.google.pik4a.view.MainActivity
 import com.google.pik4a.viewmodel.PODViewModel
 import com.google.pik4a.viewmodel.PictureOfTheDayData
 
@@ -33,6 +36,7 @@ class PODFragment : Fragment() {
     ): View? {
 
         binding = FragmentMainBinding.inflate(inflater)
+        setActionBar()
         return binding.root
     }
 
@@ -52,6 +56,19 @@ class PODFragment : Fragment() {
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
 
 
+        binding.radioGroup.setOnCheckedChangeListener { group: RadioGroup?, checkId: Int ->
+            val checkedRadioButton = group?.findViewById(group.checkedRadioButtonId) as? RadioButton
+            checkedRadioButton?.let {
+
+                if (checkedRadioButton.isChecked)
+                    Toast.makeText(
+                        requireContext(),
+                        "RadioGroup: ${group?.contentDescription} RadioButton: ${checkedRadioButton?.text}",
+                        Toast.LENGTH_LONG
+
+                    ).show()
+            }
+        }
     }
 
 
@@ -92,7 +109,61 @@ class PODFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.menu, menu)
+        inflater.inflate(R.menu.menu_bottom_bar, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.app_bar_fav -> {
+                Toast.makeText(context, "Favorite", Toast.LENGTH_SHORT).show()
+            }
+            R.id.app_bar_settings -> {
+                Toast.makeText(context, "Settings", Toast.LENGTH_SHORT).show()
+            }
+            // у нашего бургера такой вот id внутри android
+            android.R.id.home -> {
+                BottomNavigationDrawerFragment.newInstance()
+                    .show(requireActivity().supportFragmentManager, "")
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private var isMain = true
+    private fun setActionBar() {
+        (context as MainActivity).setSupportActionBar(binding.bottomAppBar)
+        setHasOptionsMenu(true)
+        binding.fab.setOnClickListener {
+            if (isMain) {
+                isMain = false
+                binding.bottomAppBar.navigationIcon = null // лучше придумать замену бургеру
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+                binding.fab.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_back_fab
+                    )
+                )
+                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar_other_screen)
+            } else {
+                isMain = true
+                binding.bottomAppBar.navigationIcon =
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_hamburger_menu_bottom_bar
+                    )
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+                binding.fab.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_plus_fab
+                    )
+                )
+                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar)
+            }
+        }
+
     }
 
 
