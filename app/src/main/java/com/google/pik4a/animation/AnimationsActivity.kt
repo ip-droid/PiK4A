a83b92b52722f9dbc9a437ed8921d131f4ff80b7
msv55 <ip-droid@mail.ru>
2021-09-28 13:05:35 +0300

    разобрался с changebounds и changeimagetransform

--- app/src/main/java/com/google/pik4a/animation/AnimationsActivity.kt
+++ app/src/main/java/com/google/pik4a/animation/AnimationsActivity.kt
@@ -1,73 +1,39 @@
 package com.google.pik4a.animation
 
-import android.graphics.Rect
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import androidx.transition.*
-import com.google.pik4a.R
-import com.google.pik4a.databinding.ActivityAnimationsExplodeBinding
+import androidx.transition.ChangeBounds
+import androidx.transition.ChangeImageTransform
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
+import com.google.pik4a.databinding.ActivityAnimationsEnlargeBinding
 
 
 class AnimationsActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityAnimationsExplodeBinding
+    lateinit var binding: ActivityAnimationsEnlargeBinding
 
-    var textIsVisible = false
+    var isExpanded = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAnimationsExplodeBinding.inflate(layoutInflater)
+        binding = ActivityAnimationsEnlargeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.recyclerView.adapter = Adapter()
-    }
+        binding.imageView.setOnClickListener {
+            isExpanded= !isExpanded
 
-    fun explode(clickedView:View){
-        val viewRect = Rect()
-        clickedView.getGlobalVisibleRect(viewRect)
-        val explode = Explode()
-        explode.epicenterCallback = object : Transition.EpicenterCallback(){
-            override fun onGetEpicenter(transition: Transition): Rect {
-                return viewRect
+            val set = TransitionSet()
+                .addTransition(ChangeBounds())
+                .addTransition(ChangeImageTransform())
+
+            TransitionManager.beginDelayedTransition(binding.container,set)
+            binding.imageView.scaleType = if(isExpanded){
+                ImageView.ScaleType.CENTER_CROP
+            }else{
+                ImageView.ScaleType.FIT_CENTER
             }
         }
-        explode.excludeTarget(clickedView,true)
-        explode.duration = 5000
-
-
-        val fade = Fade().addTarget(clickedView)
-        fade.duration = 5000
-        val setTransition = TransitionSet()
-            .addTransition(explode)
-            .addTransition(fade)
-
-        TransitionManager.beginDelayedTransition(binding.recyclerView,setTransition)
-        binding.recyclerView.adapter = null
     }
-
-    inner class Adapter : RecyclerView.Adapter<Adapter.MyViewHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-            return MyViewHolder(
-                LayoutInflater.from(parent.context).inflate(
-                    R.layout.activity_animations_explode_list_item,
-                    parent,
-                    false
-                ) as View
-            )
-        }
-        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-            holder.itemView.setOnClickListener {
-                explode(it)
-            }
-        }
-        override fun getItemCount(): Int {
-            return 32
-        }
-        inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) // FIXME
-    }
-
 
 
 }
